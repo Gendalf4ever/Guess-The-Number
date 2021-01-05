8aed9181aae3c4bafe888b4f5484dde3f272dee0
Gendalf4ever <60482216+Gendalf4ever@users.noreply.github.com>
2021-01-05 16:10:09 +0300

    выводит рандомное число от 1 до 10

--- GuessTheNumber/ViewController.swift
+++ GuessTheNumber/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
-
+import Foundation
 class ViewController: UIViewController {
-
+var guessedNumber = Int.random(in: 1...10)
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+          print(guessedNumber)
+        Answer.isHidden = true
     }
 
-
+    @IBOutlet weak var Answer: UILabel!
+    
+    @IBOutlet weak var theNumber: UITextField!
+    @IBAction func guessButton(_ sender: Any) {
+        guard let number = theNumber.text else {return}
+        let intNumber:Int? = Int(number)
+        
+        if (intNumber != guessedNumber) {
+            Answer.isHidden = false
+            Answer.text = "Неправильно!"
+            Answer.textColor = UIColor.red
+        }
+        else {
+            Answer.isHidden = false
+            Answer.text = "Правильно!"
+            Answer.textColor = UIColor.green
+        }
+    }
+    
 }
 
