7b70e9921f6b2fc11780ef849e17aff6cfe7ef6a
Gendalf4ever <60482216+Gendalf4ever@users.noreply.github.com>
2021-01-11 02:25:42 +0300

    some tests

--- GuessTheNumber/ViewController.swift
+++ GuessTheNumber/ViewController.swift
@@ -13,24 +13,41 @@ var guessedNumber = Int.random(in: 1...10)
   
     override func viewDidLoad() {
         super.viewDidLoad()
-          print(guessedNumber)
+//        print(guessedNumber)
         Answer.isHidden = true
+        someLabel.isHidden = true
     }
 
     @IBOutlet weak var Answer: UILabel!
-    
     @IBOutlet weak var someLabel: UILabel!
     @IBOutlet weak var theNumber: UITextField!
+     
+    @IBOutlet weak var upperBorderTextField: UITextField!
+    
+    @IBOutlet weak var lowerBorderTextField: UITextField!
+    
+    @IBAction func saveActionsButton(_ sender: Any) {
+        guard let upperBorder = upperBorderTextField.text else {return}
+        guard let lowerBorder = lowerBorderTextField.text else {return}
+        let upperBorderInt:UInt32? = UInt32(upperBorder)
+        let lowerBorderInt:UInt32? = UInt32(lowerBorder)
+        print("upperBorderInt: ", upperBorderInt)
+        print("lowerBorderInt: ", lowerBorderInt)
+        var random_number = Int(arc4random_uniform(upperBorderInt ?? 10) + lowerBorderInt! ?? 1 )
+           print ("random = ", random_number);
+        
+    }
     @IBAction func guessButton(_ sender: Any) {
         guard let number = theNumber.text else {return}
+    
         let intNumber:Int? = Int(number)
-        
+          someLabel.isHidden = false
         if (intNumber != guessedNumber) {
             if (intNumber ?? guessedNumber < guessedNumber){
-                 someLabel.text = "Загаданное число меньше"
+                 someLabel.text = "Загаданное число больше"
             }
            else if (intNumber ?? guessedNumber > guessedNumber){
-                           someLabel.text = "Загаданное число больше"
+                           someLabel.text = "Загаданное число меньше"
                       }
             
             Answer.isHidden = false
